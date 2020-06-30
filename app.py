6d9debcb9fa4a746fbaf1d1fcbd2339139b6b5a3
agusHendra67 <61777139+agusHendra67@users.noreply.github.com>
2020-07-01 01:55:43 +0700

    test

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import os
+import pandas as pd
 from flask import Flask, request, abort
-
+from flask.logging import create_logger 
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError, LineBotApiError
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
+LOG = create_logger(app)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('DhLYkk/1uuQ130naHtlK2g7ebRDqe+OB0rVIGgFXqyRTf3zOCNTdEwYkDbDNOYF7MJNgHK1T21nK7s3Mvy+VboMpODA9uC5LzvqdqzjmtXZrR7+LnA4Wc9RK/rqeKJAbjSVSpz9qbanDOLFJdx6qxwdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('725f080ba14b01dd9e2f0f8022afd674')
 
 
 @app.route("/callback", methods=['POST'])
@@ -24,7 +26,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    LOG.info("Request body: " + body)
 
     # handle webhook body
     try:
@@ -35,11 +37,27 @@ def callback():
     return 'OK'
 
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+
+    data = pd.DataFrame()
+    msg = (event.message.text).lower()
+
+    if ('hai' in msg) or ('hello' in msg) or ('hai' in msg) or ('hi' in msg) :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Hello pengguna!'))
+    else :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Terimakasih, tanggapan anda akan disimpan di sistem :)'))
+        
+    profile = line_bot_api.get_profile(event.user_id)
+    #waktu 
+    data['profile'] = profile
+    data['komplain_teks'] = msg
+    data.to_csv('Data Teks Komplain LINE')
 
 
 if __name__ == "__main__":
