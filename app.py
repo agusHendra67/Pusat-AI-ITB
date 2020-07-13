8e55801e1a18edf164135b5cf8593081e8b45f97
agusHendra67 <61777139+agusHendra67@users.noreply.github.com>
2020-07-13 14:29:14 +0700

    make it

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-import pandas as pd
-from flask import Flask, request, abort
+import re
+from datetime import datetime
+
+from flask import Flask, request, abort, jsonify
 from flask.logging import create_logger 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -10,16 +12,41 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
-)
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CameraAction, CameraRollAction, LocationAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    MemberJoinedEvent, MemberLeftEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    SeparatorComponent, QuickReply, QuickReplyButton,
+    ImageSendMessage)
 
+import psycopg2
+
+#DATABASE
+connection = psycopg2.connect(user="svnvojzabgxkgu",
+                              password="04eda3e5ffd827b25b1800a4898b71e048beb162f0d5b3ff9425b513284f34b7",
+                              host="ec2-52-86-33-50.compute-1.amazonaws.com",
+                              port="5432",
+                              database="d2cu9ok1h3c59n")
+cursor = connection.cursor()
+
+#app
 app = Flask(__name__)
 LOG = create_logger(app)
 
+#line API
 line_bot_api = LineBotApi('DhLYkk/1uuQ130naHtlK2g7ebRDqe+OB0rVIGgFXqyRTf3zOCNTdEwYkDbDNOYF7MJNgHK1T21nK7s3Mvy+VboMpODA9uC5LzvqdqzjmtXZrR7+LnA4Wc9RK/rqeKJAbjSVSpz9qbanDOLFJdx6qxwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('725f080ba14b01dd9e2f0f8022afd674')
 
-
-@app.route("/callback", methods=['POST'])
+#Route
+@app.route("/callback", methods=['GET','POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
@@ -33,33 +60,94 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
+    
     return 'OK'
+    
+@handler.add(FollowEvent)
+def handle_message_follow(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Halo {} selamat datang di Chatbot ITB Care, silakan pilih menu (1/2/3/4/5/6) sbb:\n1. Penyampaian masukan untuk ITB\n2. Tanya informasi fasilitas Sarana Prasarana di ITB \n3. Tanya informasi mengenai Sabuga ITB\n4. Tanya informasi perpustakaan ITB\n5. Tanya informasi Pelayanan Kesehatan ITB\n6. Online booking fasilitas'.format(profile.display_name)))
 
-
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    if event.postback.data == "1":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Silahkan masukkan keluhan anda'))
+    elif event.postback.data == "2" :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Silahkan masukkan lokasi'))
+    elif event.postback.data == "3":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Silahkan masukkan data gambar'))
+    
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
-    data = pd.DataFrame()
+    #data  
     msg = (event.message.text).lower()
+    profile = line_bot_api.get_profile(event.source.user_id)
+    gedung_kuliah = ['labtek', 'lfm', 'oktagon', 'tvst', 'gku barat', 'gku timur', 'labtek v', 'labtek 5', 'labtek vi', 'labtek 6', 'labtek i', 'labtek 1', 'bsc', 'gedung doping', 'doping', 'crcs', 'cas', 'cadl'] 
+    
+    #mencari nama gedung/tempat
+    for i in range(len(gedung_kuliah)):
+        x = re.search(gedung_kuliah[i], msg)
+        if x == None :
+            continue
+        else :
+            lokasi = x.group()
+    # data waktu
+    dates =  (datetime.fromtimestamp(event.timestamp)).strftime("%d/%m/%Y, %H:%M:%S")
 
-    if ('hai' in msg) or ('hello' in msg) or ('hai' in msg) or ('hi' in msg) :
+    if ('hai' in msg) or ('hello' in msg) or ('hai' in msg) or ('hi' in msg) or ('halo' in msg) :
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text='Hello pengguna!'))
+            TextSendMessage(text='Selamat datang di Chatbot ITB Care, silakan pilih menu (1/2/3/4/5/6) sbb:\n1. Penyampaian masukan untuk ITB\n2. Tanya informasi fasilitas Sarana Prasarana di ITB \n3. Tanya informasi mengenai Sabuga ITB\n4. Tanya informasi perpustakaan ITB\n5. Tanya informasi Pelayanan Kesehatan ITB\n6. Online booking fasilitas'))
+    elif msg == "1":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Silahkan masukkan email'))  
+    elif re.findall(".com$", msg) or re.findall(".co.id$", msg) or re.findall(".org$", msg):
+        buttons_template = ButtonsTemplate( 
+        text='Halo {}, silahkan masukkan data dibawah ya :)'.format(profile.display_name),
+        thumbnail_image_url='https://cdn.idntimes.com/content-images/community/2017/09/itb-d41de4ef55a5584eb4de86cdd085cc2d_600x400.jpg', 
+        actions=[
+            PostbackAction(label='Komplain', data='1'),
+            PostbackAction(label='Lokasi', data='2'),
+            PostbackAction(label='Foto/gambar', data='3'),
+            ])
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+        #insert data into database
+        postgres_insert_query = """ INSERT INTO public.user_profile (user_id, "line_displayName", email) VALUES (%s,%s,%s)"""
+        record_to_insert = (event.source.user_id, profile.display_name, msg, lokasi, dates)
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+ 
     else :
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text='Terimakasih, tanggapan anda akan disimpan di sistem :)'))
-        
-    profile = line_bot_api.get_profile(event.user_id)
-    #waktu 
-    data['profile'] = profile
-    data['komplain_teks'] = msg
-    data.to_csv('Data Teks Komplain LINE')
+            TextSendMessage(text='Terimakasih atas waktunya, data berhasil disimpan'))
+
+        #insert data into database
+        postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, teks_komplain, lokasi, waktu_komplain) VALUES (%s,%s,%s,%s,%s)"""
+        record_to_insert = (event.source.user_id, event.message.id, msg, lokasi, dates)
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+    
+            
+@handler.add(MessageEvent, message=(ImageMessage))
+def handle_message_image(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Terimakasih atas waktunya, gambar berhasil disimpan'))
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
