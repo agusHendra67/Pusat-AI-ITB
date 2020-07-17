7f501279c1dc2a10ba7c4894f63640212680b09f
agusHendra67 <61777139+agusHendra67@users.noreply.github.com>
2020-07-17 22:08:29 +0700

    tes

--- app.py
+++ app.py
@@ -147,18 +147,18 @@ def handle_message(event):
             
 @handler.add(MessageEvent, message=(ImageMessage))
 def handle_message_image(event):
-    img = (line_bot_api.get_message_content(event.message_id)).content()
-    waktu = (datetime.fromtimestamp(event.timestamp/1e3)).strftime("%m/%d/%Y, %H:%M:%S")
-
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='Terimakasih atas waktunya, gambar berhasil disimpan'))
 
-    # #insert database
-    postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, waktu_komplain, gambar) VALUES (%s,%s,%s,%s)"""
-    record_to_insert = (event.source.user_id, event.message.id, waktu, img)
-    cursor.execute(postgres_insert_query, record_to_insert)
-    connection.commit()
+    img = (line_bot_api.get_message_content(event.message.id)).content()
+    waktu = (datetime.fromtimestamp(event.timestamp/1e3)).strftime("%m/%d/%Y, %H:%M:%S")
+
+    # # #insert database
+    # postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, waktu_komplain, gambar) VALUES (%s,%s,%s,%s)"""
+    # record_to_insert = (event.source.user_id, event.message.id, waktu, img)
+    # cursor.execute(postgres_insert_query, record_to_insert)
+    # connection.commit()
 
         
 
