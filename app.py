397dc7d750cd731ad2d059f6d8027143faf8afcf
agusHendra67 <61777139+agusHendra67@users.noreply.github.com>
2020-07-22 01:00:12 +0700

    nice

--- app.py
+++ app.py
@@ -142,9 +142,15 @@ def handle_message(event):
 
         #waktu
         waktu = (datetime.fromtimestamp(event.timestamp/1e3).astimezone(tz= pytz.timezone('Asia/Jakarta'))).strftime("%m/%d/%Y, %H:%M:%S")
-          
+
+        #get image as binary 
+        try :
+            img = line_bot_api.get_message_content().content()
+        except :
+            img = None 
+
         #insert data into database
-        postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, teks_komplain, lokasi, waktu_komplain) VALUES (%s,%s,%s,%s,%s)"""
+        postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, teks_komplain, lokasi, waktu_komplain, gambar) VALUES (%s,%s,%s,%s,%s,%s)"""
         record_to_insert = (event.source.user_id, event.message.id, msg, lokasi, waktu)
         cursor.execute(postgres_insert_query, record_to_insert)
         connection.commit()
@@ -159,19 +165,19 @@ def handle_message_image(event):
         TextSendMessage(text='Terimakasih atas waktunya, gambar berhasil disimpan'))
 
     
-    #get image as binary 
-    r = requests.get('https://api-data.line.me/v2/bot/message/{}/content'.format(event.message.id), headers={'Authorization' : 'Bearer DhLYkk/1uuQ130naHtlK2g7ebRDqe+OB0rVIGgFXqyRTf3zOCNTdEwYkDbDNOYF7MJNgHK1T21nK7s3Mvy+VboMpODA9uC5LzvqdqzjmtXZrR7+LnA4Wc9RK/rqeKJAbjSVSpz9qbanDOLFJdx6qxwdB04t89/1O/w1cDnyilFU='} )
-    img = r.content
+    # #get image as binary 
+    # r = requests.get('https://api-data.line.me/v2/bot/message/{}/content'.format(event.message.id), headers={'Authorization' : 'Bearer DhLYkk/1uuQ130naHtlK2g7ebRDqe+OB0rVIGgFXqyRTf3zOCNTdEwYkDbDNOYF7MJNgHK1T21nK7s3Mvy+VboMpODA9uC5LzvqdqzjmtXZrR7+LnA4Wc9RK/rqeKJAbjSVSpz9qbanDOLFJdx6qxwdB04t89/1O/w1cDnyilFU='} )
+    # img = r.content
 
-    #waktu
-    waktu = (datetime.fromtimestamp(event.timestamp/1e3).astimezone(tz= pytz.timezone('Asia/Jakarta'))).strftime("%m/%d/%Y, %H:%M:%S")
+    # #waktu
+    # waktu = (datetime.fromtimestamp(event.timestamp/1e3).astimezone(tz= pytz.timezone('Asia/Jakarta'))).strftime("%m/%d/%Y, %H:%M:%S")
 
     
-    #insert image into database
-    postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, waktu_komplain, gambar) VALUES (%s,%s,%s,%s)"""
-    record_to_insert = (event.source.user_id, event.message.id, waktu, img)
-    cursor.execute(postgres_insert_query, record_to_insert)
-    connection.commit()
+    # #insert image into database
+    # postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, waktu_komplain, gambar) VALUES (%s,%s,%s,%s)"""
+    # record_to_insert = (event.source.user_id, event.message.id, waktu, img)
+    # cursor.execute(postgres_insert_query, record_to_insert)
+    # connection.commit()
 
         
 
