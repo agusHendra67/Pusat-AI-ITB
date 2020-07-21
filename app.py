aae6c60905fc1709cb13d5af7d96a40ae771c113
agusHendra67 <61777139+agusHendra67@users.noreply.github.com>
2020-07-21 22:01:03 +0700

    coba

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def handle_message_image(event):
     #     img = None
     #gambar
     r = requests.get('https://api-data.line.me/v2/bot/message/{}/content'.format(event.message.id), headers={'Authorization' : 'Bearer DhLYkk/1uuQ130naHtlK2g7ebRDqe+OB0rVIGgFXqyRTf3zOCNTdEwYkDbDNOYF7MJNgHK1T21nK7s3Mvy+VboMpODA9uC5LzvqdqzjmtXZrR7+LnA4Wc9RK/rqeKJAbjSVSpz9qbanDOLFJdx6qxwdB04t89/1O/w1cDnyilFU='} )
-    img = (r.content).hex
+    img = r.content
 
     #waktu
     waktu = (datetime.fromtimestamp(event.timestamp/1e3).astimezone(tz= pytz.timezone('Asia/Jakarta'))).strftime("%m/%d/%Y, %H:%M:%S")
