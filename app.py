8b540a06e8a460dfa5ced16cafe6b2bf891142a0
agusHendra67 <61777139+agusHendra67@users.noreply.github.com>
2020-07-21 22:06:06 +0700

    coba

--- app.py
+++ app.py
@@ -159,8 +159,8 @@ def handle_message_image(event):
     # except :
     #     img = None
     #gambar
-    r = requests.get('https://api-data.line.me/v2/bot/message/{}/content'.format(event.message.id), headers={'Authorization' : 'Bearer DhLYkk/1uuQ130naHtlK2g7ebRDqe+OB0rVIGgFXqyRTf3zOCNTdEwYkDbDNOYF7MJNgHK1T21nK7s3Mvy+VboMpODA9uC5LzvqdqzjmtXZrR7+LnA4Wc9RK/rqeKJAbjSVSpz9qbanDOLFJdx6qxwdB04t89/1O/w1cDnyilFU='} )
-    img = r.content
+    # r = requests.get('https://api-data.line.me/v2/bot/message/{}/content'.format(event.message.id), headers={'Authorization' : 'Bearer DhLYkk/1uuQ130naHtlK2g7ebRDqe+OB0rVIGgFXqyRTf3zOCNTdEwYkDbDNOYF7MJNgHK1T21nK7s3Mvy+VboMpODA9uC5LzvqdqzjmtXZrR7+LnA4Wc9RK/rqeKJAbjSVSpz9qbanDOLFJdx6qxwdB04t89/1O/w1cDnyilFU='} )
+    # img = r.content
 
     #waktu
     waktu = (datetime.fromtimestamp(event.timestamp/1e3).astimezone(tz= pytz.timezone('Asia/Jakarta'))).strftime("%m/%d/%Y, %H:%M:%S")
@@ -170,8 +170,8 @@ def handle_message_image(event):
         TextSendMessage(text='Terimakasih atas waktunya, gambar berhasil disimpan'))
 
     #insert database
-    postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, waktu_komplain, gambar) VALUES (%s,%s,%s,%s)"""
-    record_to_insert = (event.source.user_id, event.message.id, waktu, img)
+    postgres_insert_query = """ INSERT INTO public.komplain (user_id, message_id, waktu_komplain) VALUES (%s,%s,%s)"""
+    record_to_insert = (event.source.user_id, event.message.id, waktu)
     cursor.execute(postgres_insert_query, record_to_insert)
     connection.commit()
 
